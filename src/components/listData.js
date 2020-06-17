1ce51387f4403316fe1d3c8cad4085581a4b9d74
samnkrki <samn_krki@live.com>
2020-06-17 12:01:44 +0545

    added modal to view data, see all button to see Data which opens modal

--- src/components/listData.js
+++ src/components/listData.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import { Modal } from 'react-bootstrap'
+import AddData from './addData';
 
 export default function ListData() {
+    const [userList, setUserList] = useState([]);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const options = {
+        paginationSize: 4,
+        firstPageText: 'First',
+        prePageText: 'Back',
+        nextPageText: 'Next',
+        lastPageText: 'Last',
+    }
     useEffect(() => {
-        const url = "http://localhost:3000"
+        const url = "http://localhost:3001"
+        fetchList(url)
     }, [])
-    const data = [
-        {
-            name: "",
-            educationBackground: "",
-            email: "",
-            gender: "",
-            dob: "",
-            nationality: "",
-            phone: "",
-            prefModeContact: "",
-            address: ""
-        }
-    ]
+
+    const fetchList = async (url) => {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        let res = response.json()
+        res
+            .then(data => {
+                if (data.success == true) {
+                    setUserList([...data.data])
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     const columns = [
         {
             dataField: 'name',
@@ -41,15 +65,58 @@ export default function ListData() {
         },
         {
             dataField: 'educationBackground',
-            text: 'Educational Background'
+            text: 'Education Background'
         },
         {
             dataField: 'dob',
             text: 'Date of Birth'
+        },
+        {
+            dataField: 'address',
+            text: 'Address'
+        },
+        {
+            dataField: 'prefModeContact',
+            text: 'Preferred Mode of Contact'
+        }, {
+            dataField: 'df1',
+            text: "Action",
+            formatter: (cellContent, row) => {
+                return (
+                    <button onClick={() => clickToSeeAll(cellContent, row)}>See all</button>
+                )
+            }
         }
+
     ];
 
+    const clickToSeeAll = (cellContent, row) => {
+        console.log(row, cellContent)
+        handleShow()
+    }
     return (
-        <BootstrapTable data={data} columns={columns} keyField='email' />
+        <>
+            <BootstrapTable
+                data={userList}
+                columns={columns}
+                keyField='_id'
+                noDataIndication={noDataText()}
+                pagination={paginationFactory(options)}
+
+            />
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>View Details</Modal.Title>
+                </Modal.Header>
+                <Modal.Body><AddData /></Modal.Body>
+            </Modal>
+        </>
+
+    )
+}
+
+const noDataText = () => {
+    return (
+        <div>Empty! Add data to see them here.</div>
     )
 }
