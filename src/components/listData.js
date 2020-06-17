e9c8232d51d9ad3a432ee6ed2939ff3310de76b3
samnkrki <samn_krki@live.com>
2020-06-17 13:09:02 +0545

    added router link to add page, and dissplay details page with modal

--- src/components/listData.js
+++ src/components/listData.js
@@ -3,13 +3,15 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import { Modal } from 'react-bootstrap'
 import AddData from './addData';
+import { useHistory } from 'react-router-dom';
 
 export default function ListData() {
     const [userList, setUserList] = useState([]);
     const [show, setShow] = useState(false);
-
+    const [dataToShow, setDataToShow] = useState({})
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleShow = (details) => { setShow(true); setDataToShow(details) };
+    const history = useHistory()
     const options = {
         paginationSize: 4,
         firstPageText: 'First',
@@ -91,8 +93,11 @@ export default function ListData() {
     ];
 
     const clickToSeeAll = (cellContent, row) => {
-        console.log(row, cellContent)
-        handleShow()
+        // console.log(row, cellContent)
+        handleShow(row)
+    }
+    const navigateToLists = () => {
+        history.push("/add");
     }
     return (
         <>
@@ -104,11 +109,12 @@ export default function ListData() {
                 pagination={paginationFactory(options)}
 
             />
+            <button type="button" className=" btn btn-secondary" onClick={() => navigateToLists()}>Add new data</button>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>View Details</Modal.Title>
                 </Modal.Header>
-                <Modal.Body><AddData /></Modal.Body>
+                <Modal.Body><AddData details={dataToShow} /></Modal.Body>
             </Modal>
         </>
 
