3deb390b1667815a5f629fb52528ce4b55cf0fe9
samnkrki <samn_krki@live.com>
2020-06-18 17:25:47 +0545

    added heroku baseurl

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:3001/"
+// export const baseUrl = "http://localhost:3001/"
+export const baseUrl = "https://taskapi101.herokuapp.com/"
 export const saveDataUrl = "saveInformation"
 export const getDataUrl = "getLists"
 export const headers = {
