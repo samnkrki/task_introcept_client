05cb5625000c10a9e500de31d97b07836bb88d22
samnkrki <samn_krki@live.com>
2020-06-18 09:41:03 +0545

    added headers for all requests

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,3 +1,7 @@
 export const baseUrl = "http://localhost:3001/"
 export const saveDataUrl = "saveInformation"
 export const getDataUrl = "getLists"
+export const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+}
